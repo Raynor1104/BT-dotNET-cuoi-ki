7562ce893808fc84cdd8a5dac25d502fba6ec9b1
Raynor1104 <66164155+Raynor1104@users.noreply.github.com>
2022-06-18 20:27:39 +0700

    Add files via upload

---
+++ Form Quân đội/Menu.Designer.cs
@@ -0,0 +1,427 @@
+﻿namespace Form_Quân_đội
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_id_add = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_name_add = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_dob_add = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_rank_add = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_unit_add = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_write = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_unit = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_rank = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_dob = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox_score_add = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox_score = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBox_grade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(176, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(735, 177);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // textBox_id_add
+            // 
+            this.textBox_id_add.Location = new System.Drawing.Point(55, 39);
+            this.textBox_id_add.Name = "textBox_id_add";
+            this.textBox_id_add.Size = new System.Drawing.Size(100, 20);
+            this.textBox_id_add.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Name:";
+            // 
+            // textBox_name_add
+            // 
+            this.textBox_name_add.Location = new System.Drawing.Point(55, 65);
+            this.textBox_name_add.Name = "textBox_name_add";
+            this.textBox_name_add.Size = new System.Drawing.Size(100, 20);
+            this.textBox_name_add.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "DOB:";
+            // 
+            // textBox_dob_add
+            // 
+            this.textBox_dob_add.Location = new System.Drawing.Point(55, 91);
+            this.textBox_dob_add.Name = "textBox_dob_add";
+            this.textBox_dob_add.Size = new System.Drawing.Size(100, 20);
+            this.textBox_dob_add.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Rank:";
+            // 
+            // textBox_rank_add
+            // 
+            this.textBox_rank_add.Location = new System.Drawing.Point(55, 117);
+            this.textBox_rank_add.Name = "textBox_rank_add";
+            this.textBox_rank_add.Size = new System.Drawing.Size(100, 20);
+            this.textBox_rank_add.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Unit:";
+            // 
+            // textBox_unit_add
+            // 
+            this.textBox_unit_add.Location = new System.Drawing.Point(55, 143);
+            this.textBox_unit_add.Name = "textBox_unit_add";
+            this.textBox_unit_add.Size = new System.Drawing.Size(100, 20);
+            this.textBox_unit_add.TabIndex = 18;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(64, 195);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 20;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(989, 224);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 21;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(286, 224);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 22;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_write
+            // 
+            this.btn_write.Location = new System.Drawing.Point(691, 224);
+            this.btn_write.Name = "btn_write";
+            this.btn_write.Size = new System.Drawing.Size(75, 23);
+            this.btn_write.TabIndex = 23;
+            this.btn_write.Text = "Write";
+            this.btn_write.UseVisualStyleBackColor = true;
+            this.btn_write.Click += new System.EventHandler(this.btn_write_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(942, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "Unit:";
+            // 
+            // textBox_unit
+            // 
+            this.textBox_unit.Location = new System.Drawing.Point(977, 146);
+            this.textBox_unit.Name = "textBox_unit";
+            this.textBox_unit.Size = new System.Drawing.Size(100, 20);
+            this.textBox_unit.TabIndex = 32;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(935, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Rank:";
+            // 
+            // textBox_rank
+            // 
+            this.textBox_rank.Location = new System.Drawing.Point(977, 120);
+            this.textBox_rank.Name = "textBox_rank";
+            this.textBox_rank.Size = new System.Drawing.Size(100, 20);
+            this.textBox_rank.TabIndex = 30;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(938, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "DOB:";
+            // 
+            // textBox_dob
+            // 
+            this.textBox_dob.Location = new System.Drawing.Point(977, 94);
+            this.textBox_dob.Name = "textBox_dob";
+            this.textBox_dob.Size = new System.Drawing.Size(100, 20);
+            this.textBox_dob.TabIndex = 28;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(933, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Name:";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(977, 68);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(100, 20);
+            this.textBox_name.TabIndex = 26;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(950, 45);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "ID:";
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(977, 42);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(100, 20);
+            this.textBox_id.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(11, 172);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(38, 13);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "Score:";
+            // 
+            // textBox_score_add
+            // 
+            this.textBox_score_add.Location = new System.Drawing.Point(55, 169);
+            this.textBox_score_add.Name = "textBox_score_add";
+            this.textBox_score_add.Size = new System.Drawing.Size(100, 20);
+            this.textBox_score_add.TabIndex = 34;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(933, 172);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(38, 13);
+            this.label14.TabIndex = 39;
+            this.label14.Text = "Score:";
+            // 
+            // textBox_score
+            // 
+            this.textBox_score.Location = new System.Drawing.Point(977, 172);
+            this.textBox_score.Name = "textBox_score";
+            this.textBox_score.Size = new System.Drawing.Size(100, 20);
+            this.textBox_score.TabIndex = 38;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(932, 201);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(39, 13);
+            this.label15.TabIndex = 41;
+            this.label15.Text = "Grade:";
+            // 
+            // textBox_grade
+            // 
+            this.textBox_grade.Location = new System.Drawing.Point(977, 198);
+            this.textBox_grade.Name = "textBox_grade";
+            this.textBox_grade.Size = new System.Drawing.Size(100, 20);
+            this.textBox_grade.TabIndex = 40;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Add form:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(986, 23);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(84, 16);
+            this.label13.TabIndex = 43;
+            this.label13.Text = "Update form:";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 261);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox_grade);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox_score);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox_score_add);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_unit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_rank);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_dob);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.btn_write);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_unit_add);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_rank_add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_dob_add);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_name_add);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_id_add);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_id_add;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_name_add;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_dob_add;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_rank_add;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_unit_add;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_write;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_unit;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_rank;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_dob;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox_score_add;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox_score;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox_grade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Form Quân đội/Menu.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Form_Quân_đội
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public static DataTable table = new DataTable();
+
+        int rowIndex;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Date of birth", typeof(DateTime));
+            table.Columns.Add("Rank", typeof(string));
+            table.Columns.Add("Unit", typeof(string));
+            table.Columns.Add("Score", typeof(int));
+            table.Columns.Add("Grade", typeof(string));
+
+            dataGridView1.DataSource = table;
+
+            string[] lines = File.ReadAllLines("soldier.txt");
+            string[] words;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                words = lines[i].ToString().Split(',');
+                table.Rows.Add(words);
+            }
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                MessageBox.Show(dataGridView1.SelectedColumns.ToString());
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rowIndex = e.RowIndex;
+            if (rowIndex >= 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[rowIndex];
+                string id = selectedRow.Cells[0].Value.ToString();
+                string name = selectedRow.Cells[1].Value.ToString();
+                string dOB = selectedRow.Cells[2].Value.ToString();
+                string rank = selectedRow.Cells[3].Value.ToString();
+                string unit = selectedRow.Cells[4].Value.ToString();
+                int score = Convert.ToInt32(selectedRow.Cells[5].Value.ToString());
+                
+                string gradeValue;
+                if (score >= 0 && score <= 100)
+                {
+                    if (score >= 90 && score <= 100)
+                        gradeValue = "S";
+                    else if (score >= 70 && score < 90)
+                        gradeValue = "A";
+                    else if (score >= 60 && score < 70)
+                        gradeValue = "B";
+                    else if (score >= 50 && score < 60)
+                        gradeValue = "C";
+                    else
+                        gradeValue = "D";
+                }
+                else
+                {
+                    gradeValue = "Exceed Score";
+                }
+                selectedRow.Cells[6].Value = gradeValue.ToString();
+
+                textBox_id.Text = id;
+                textBox_id.ReadOnly = true;
+                textBox_name.Text = name;
+                textBox_dob.Text = dOB;
+                textBox_rank.Text = rank;
+                textBox_unit.Text = unit;
+                textBox_score.Text = score.ToString();
+                textBox_grade.Text = selectedRow.Cells[6].Value.ToString();
+                textBox_grade.ReadOnly = true;
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(Convert.ToInt32(textBox_id_add.Text), 
+                           textBox_name_add.Text, 
+                           textBox_dob_add.Text,
+                           textBox_rank_add.Text,
+                           textBox_unit_add.Text,
+                           textBox_score_add.Text
+                          );
+            dataGridView1.DataSource = table;
+            MessageBox.Show("Staff added.");
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Update row index: {rowIndex}");
+
+            table.Rows[rowIndex].SetField(1, textBox_name.Text);
+            table.Rows[rowIndex].SetField(2, textBox_dob.Text);
+            table.Rows[rowIndex].SetField(3, textBox_rank.Text);
+            table.Rows[rowIndex].SetField(4, textBox_unit.Text);
+            table.Rows[rowIndex].SetField(5, textBox_score.Text);
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Deleted row index: {rowIndex}");
+            table.Rows[rowIndex].Delete();
+        }
+
+        private void btn_write_Click(object sender, EventArgs e)
+        {
+            TextWriter writer = new StreamWriter("soldier2.txt");
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    writer.Write("\t" + dataGridView1.Rows[i].Cells[j].Value.ToString() + "\t" + "|");
+                }
+                writer.WriteLine("");
+                writer.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+            }
+            writer.Close();
+            MessageBox.Show("Data Written.");
+        }
+    }
+}
